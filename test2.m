9eec2a18b0d968fe20d5081059abca4cdb8c731e
Siddharth R <sidharthr0309@gmail.com>
2024-11-05 14:41:41 +0530

    Added comments to improve readability

--- test2.m
+++ test2.m
@@ -1,59 +1,61 @@
 import fso.outdoor.ao
 
-plt_resolution = 101;
-dfs = linspace(0,0.5,plt_resolution);
-etas = zeros(plt_resolution,1);
-
+% plt_resolution = 101;
+% dfs = linspace(0,0.5,plt_resolution);
+% etas = zeros(plt_resolution,1);
+% 
 % mu = [0 0];
 % sigma = [3e-6 0;0 3e-6];
-% fn = normal_phase(x,y,mu,sigma,1e-4);
-
-for a = 1:plt_resolution
-
-    sys = ao;
-    f = (12e-3)/dfs(a);
-    w0 = 8e-6;
-    w = (1550e-9)*(f)/(pi*w0);
-
-    [x,y] = sys.receiver(sys.D/2 ,sys.D/2 ,101 ,101);
-
-    phi = sys.received_wf(101,101,1,false);
-
-    ea = sys.receiver_field(phi);
-    fa = sys.of_mode_field(x,y,w);
-
-    eta = sys.coupling_efficiency(ea,fa);
-    etas(a) = eta*100;
-end
-
-plot(dfs,etas);
+% fn = normal_phase(x,y,mu,sigma);
+% 
+% for a = 1:plt_resolution
+% 
+%     sys = ao;
+%     f = (12e-3)/dfs(a);
+%     w0 = 8e-6;
+%     w = (1550e-9)*(f)/(pi*w0);
+% 
+%     [x,y] = sys.receiver(sys.D/2 ,sys.D/2 ,101 ,101);
+% 
+%     phi = sys.received_wf(101,101,1,false);
+% 
+%     ea = sys.receiver_field(phi);
+%     fa = sys.of_mode_field(x,y,w);
+% 
+%     eta = sys.coupling_efficiency(ea,fa);
+%     etas(a) = eta*100;
+% end
+% 
+% plot(dfs,etas);
 
 
 %% Single run
-sys = ao;
-df = input("Enter D/f: ");
-f = (12e-3)/df;
-w0 = 8e-6;
-w = (1550e-9)*(f)/(pi*w0);
-
-mu = [0 0];
-sigma = [3e-6 0;0 3e-6];
+%import fso.outdoor.ao
+% sys = ao;
+% df = input("Enter D/f: ");
+% f = (12e-3)/df;
+% w0 = 8e-6;
+% w = (1550e-9)*(f)/(pi*w0);
+% 
+% mu = [0 0];
+% sigma = [3e-6 0;0 3e-6];
 % fn = normal_phase(x,y,mu,sigma);
-
-[x,y] = sys.receiver(sys.D/2 ,sys.D/2 ,101 ,101);
-
-phi = sys.received_wf(101,101,1,false);
-
-ea = sys.receiver_field(phi);
-fa = sys.of_mode_field(x,y,w);
-
-eta = sys.coupling_efficiency(ea,fa);
-
-disp(eta*100);
+% 
+% [x,y] = sys.receiver(sys.D/2 ,sys.D/2 ,101 ,101);
+% 
+% phi = sys.received_wf(101,101,1,false);
+% 
+% ea = sys.receiver_field(phi);
+% fa = sys.of_mode_field(x,y,w);
+% 
+% eta = sys.coupling_efficiency(ea,fa);
+% 
+% disp(eta*100);
 
 
 %% Zernike Fitting
 
+import fso.outdoor.ao
 N = input("Enter N: ");
 sys = ao;
 [x,y] = sys.receiver(sys.D/2 ,sys.D/2 ,101 ,101);
@@ -74,10 +76,18 @@ for k = 1:N
 end
 
 %% Zernike Fitting Error Plot
-d = input("Receiver divisions : ");
-N = input("Enter max number of Zernike modes : ");
+
+import fso.outdoor.ao
+% d = input("Receiver divisions : ");
+d = 201;
+% N = input("Enter max number of Zernike modes : ");
+N = 40;
 sys = ao;
+
+% Define receiver
 [x,y] = sys.receiver(sys.D/2,sys.D/2,d,d);
+
+% For modeling received wavefront as gaussian
 mu = [0 0];
 sigma = [3e-6 0;0 3e-6];
 
@@ -87,29 +97,40 @@ sigma = [3e-6 0;0 3e-6];
 % fn = normal_phase(x,y,mu,sigma).*logNoise;
 
 % Gamma-Gamma Noise
-alpha = 3;
-beta = 2;
+alpha = 1e3;
+beta = 1e3;
 g1 = gamrnd(alpha,1/alpha,[d,d]);
 g2 = gamrnd(beta,1/beta,[d,d]);
 fn = normal_phase(x,y,mu,sigma).*(g1.*g2);
 
+% Adding AWGN
+fn = fn./max(max(fn))+rand(d,d)*3e-2;
+%Normalizing wavefront
+fn_norm = fn./(max(max(fn)));
 
-fn_norm = fn./max(max(fn));
-phi = sys.received_wf(d,d,fn_norm);
+% figure();
+% noise = g1.*g2;
+% noise = noise./max(max(noise));
+% surf(fn./max(max(fn))-noise);
+% figure();
+% surf(fn_norm);
 
-surf(fn_norm);
-
+% Vector for storing mean error, mean of abosute error and standard
+% deviation respectively
 m = zeros(1,N);
 am = zeros(1,N);
 s = zeros(1,N);
 
 for i = 1:N
-    [coefficients, zernikeModes] = sys.fitZernikeModes(phi,i);
-    zernike_surface = zeros(size(phi));
+    [coefficients, zernikeModes] = sys.fitZernikeModes(fn_norm,i);
+    zernike_surface = zeros(size(fn_norm));
 
+    % finding the corrected wavefront
     for k = 1:i
         zernike_surface = zernike_surface + coefficients(k) * zernikeModes{k};
     end
+
+    % flattening the wavefront for calculation
     err = reshape(zernike_surface-fn_norm,1,[]);
     am(i) = mean(abs(err));
     m(i) = mean(err);
@@ -126,6 +147,7 @@ plot(1:N,am);
 
 %% Plot Zernike Surfaces
 
+import fso.outdoor.ao
 sys = ao;
 rows = 201;
 cols = 201;
